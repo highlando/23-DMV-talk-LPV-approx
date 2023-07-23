e7c49a877c6a6bc6b8a99ec8239bedb60e343580
Jan Heiland <jnhlnd@gmail.com>
2023-07-23 16:17:21 +0200

    fixfix

--- prop-to-manu-fix.sh
+++ prop-to-manu-fix.sh
@@ -1,14 +1,14 @@
-git checkout master
+git checkout 23-SIAM-CT
 git add slides.md
 git commit -m 'updated the slides md'
 
 git checkout pandoc-outputs
-git merge master
+git merge 23-SIAM-CT
 ./mkdc.sh
 git add index.html
 git commit -m 'updated the slides md and now the html'
 LSTCHSH=$(git rev-parse HEAD)
 
-git checkout manu-fix-html
+git checkout manu-fix-htmls
 git cherry-pick $LSTCHSH  # merge only the last commit
 # git push gh-origin gh-pages
