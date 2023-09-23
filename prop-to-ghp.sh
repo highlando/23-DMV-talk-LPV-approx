a0cfc728d0cc33d6c5e0556cac315c6e2f7599a6
Jan Heiland <jnhlnd@gmail.com>
2023-09-23 10:08:21 +0200

    upds for dmv

--- prop-to-ghp.sh
+++ prop-to-ghp.sh
@@ -1,9 +1,9 @@
-git checkout master
+git checkout 23-DMV
 git add slides.md
 git commit -m 'updated the slides md'
 
 git checkout pandoc-outputs
-git merge master
+git merge 23-DMV
 ./mkdc.sh
 git add index.html
 git commit -m 'updated the slides md and now the html'
